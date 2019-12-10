f5259a0f645f4a0a418553be14154dcc45e8d9a3
willchou0614 <willchou0614@gmail.com>
2019-12-10 19:30:57 +0800

    table呈現meeting_info

--- meeting_info.php
+++ meeting_info.php
@@ -7,7 +7,7 @@
     <!-- Required meta tags -->
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
-
+	<link rel='stylesheet' href='style.css'>
     <!-- Bootstrap CSS -->
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
 
@@ -43,21 +43,18 @@
 
 			
 		</div>
-
-		<div class='row align-items-center justify-content-center'>
-			<div class = 'col-2 text-center'>
-			Date
-			</div>
-			<div class = 'col-2 text-center'>
-			Time
-			</div>
-			<div class = 'col-2 text-center'>
-			Discription
-			</div>
-			<div class = 'col-2 text-center'>
-			Duration
-			</div>
-		</div>
+	<div class='col custom-table-width'style='  '>
+	
+		<table class='table table-striped 'style='width:80%;margin-left:10%  '>
+    	<thead>
+    	<tr>
+      	<th scope='col'>Date</th>
+      	<th scope='col'>Time</th>
+      	<th scope='col'>Discription</th>
+      	<th scope='col'>Duration</th>
+    	</tr>
+  		</thead>
+		
 	
 		
 	
@@ -74,14 +71,47 @@
     $stmt = $db->prepare($query);
     $stmt -> execute();
     $result = $stmt -> fetchAll();
+    echo"<tbody >";
     foreach($result as $this_row)
     {
-        echo "<div class = 'row align-items-center justify-content-center'>";
-        echo "<div class = 'col-2 text-center'>".$this_row['date']."</div>";
-        echo "<div class = 'col-2 text-center'>".$this_row['time']."</div>";
-        echo "<div class = 'col-2 text-center'>".$this_row['description']."</div>";
-        echo "<div class = 'col-2 text-center'>".$this_row['duration']."</div>";
-        echo "</div>";
+    	//echo"<table class='table table-striped'>";
+    	/*echo"<thead>";
+    	echo"<tr>";
+      	echo"<th scope='col'>".$this_row['date']."</th>";
+      	echo"<th scope='col'>".$this_row['time']."</th>";
+      	echo"<th scope='col'>".$this_row['description']."</th>";
+      	echo"<th scope='col'>".$this_row['duration']."</th>";
+    	echo"</tr>";
+  		echo"</thead>";*/
+  		
+  	
+
+  	/*switch($this_row['importance'])//邱:不要刪
+    	{
+    		case 0:echo"<tr style='background-color:#ff8a8a'>";break;
+    		case 1:echo"<tr style='background-color:#faf8b6'>";break;
+    		case 2:echo"<tr style='background-color:#cbffbf'>";break;
+    		case 3:echo"<tr style='background-color:#9eecff'>";break;
+    		case 4:echo"<tr style='background-color:#9eecff'>";break;
+    	}
+    */
+    	echo"<tr>";
+      	echo"<th scope='row'>".$this_row['date']."</th>";
+      	echo"<td >".$this_row['time']."</td>";
+      	echo"<td >".$this_row['description']."</td>";
+      	echo"<td >".$this_row['duration']."</td>";
+    	echo"</tr>";
+  		
+
+      
+
+       
     }
+    echo"</tbody>";
+  
+		echo"</table>";
     echo "</div>";
+echo "</div>";
+
+
 ?>
