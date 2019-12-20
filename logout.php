8bdab54e510142de28f5f4f6fa4788312bbd532a
zhouharry <40146769+zhouharry@users.noreply.github.com>
2019-12-20 17:54:17 +0800

    update logout.php

--- logout.php
+++ logout.php
@@ -6,14 +6,10 @@ include_once "db_conn.php";
 
 $account = $_SESSION['account'];
 unset($_SESSION['account']);
-$query = ("update admin set is_online = 0 WHERE account = $account");
+$query = ("UPDATE admin SET is_online = 0 WHERE account = $account");
 $stmt = $db -> prepare($query);
 $stmt->execute();
 echo $account;
 echo '登出中......';
-$query = "UPDATE admin SET is_online=0 where account = $account";
-$stmt=$db->prepare($query);
-$stmt->execute();
-$result=$stmt->fetchALL();
 echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
 ?>
