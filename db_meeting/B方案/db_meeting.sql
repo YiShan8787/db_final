0b465eacc9374f190225812bfd6c6cd116433e53
zhouharry <40146769+zhouharry@users.noreply.github.com>
2019-12-24 14:51:03 +0800

    update database

--- db_meeting/B方案/db_meeting.sql
+++ db_meeting/B方案/db_meeting.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 
--- 伺服器版本： 10.4.8-MariaDB
--- PHP 版本： 7.3.11
+-- 產生時間： 2019 年 12 月 24 日 07:50
+-- 伺服器版本： 10.4.10-MariaDB
+-- PHP 版本： 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,27 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `db_meeting`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admin`
+--
+
+CREATE TABLE `admin` (
+  `account` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `is_online` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`account`, `password`, `is_online`) VALUES
+('123', '123', 0),
+('444', '444', 0),
+('555', '555', 0);
 
 -- --------------------------------------------------------
 
@@ -45,7 +66,8 @@ CREATE TABLE `meeting_info` (
   `date` date NOT NULL,
   `time` time(6) NOT NULL,
   `description` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `duration` int(5) NOT NULL
+  `duration` int(5) NOT NULL,
+  `announcer` varchar(15) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -79,6 +101,12 @@ CREATE TABLE `teacher` (
 --
 
 --
+-- 資料表索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`account`);
+
+--
 -- 資料表索引 `meeting_content`
 --
 ALTER TABLE `meeting_content`
@@ -89,7 +117,8 @@ ALTER TABLE `meeting_content`
 -- 資料表索引 `meeting_info`
 --
 ALTER TABLE `meeting_info`
-  ADD PRIMARY KEY (`date`,`time`);
+  ADD PRIMARY KEY (`date`,`time`),
+  ADD KEY `announcer` (`announcer`);
 
 --
 -- 資料表索引 `student`
@@ -112,6 +141,12 @@ ALTER TABLE `teacher`
 --
 ALTER TABLE `meeting_content`
   ADD CONSTRAINT `date_time_FK` FOREIGN KEY (`date`,`time`) REFERENCES `meeting_info` (`date`, `time`);
+
+--
+-- 資料表的限制式 `meeting_info`
+--
+ALTER TABLE `meeting_info`
+  ADD CONSTRAINT `announcer_FK` FOREIGN KEY (`announcer`) REFERENCES `student` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
